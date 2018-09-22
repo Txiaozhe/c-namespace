1268b8aeadc65a0abbe45a52c73a9da5f34c91d3
TangXiaoji <txiaozhe@gmail.com>
2018-09-22 22:42:37 +0800

    quick sort

--- sort/sort.c
+++ sort/sort.c
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "time.h"
-
 // 插入排序
 void insert_sort(int *a, int length) {
     int tmp;
@@ -12,6 +11,43 @@ void insert_sort(int *a, int length) {
         }
         a[j + 1] = tmp;
     }
+}
+
+// 快速排序，挖坑填数
+int partion(int *a, int start, int end) {
+	int i = start, j = end;
+	int tmp = a[i]; // 越界检查
+
+	while(i < j) {
+		// 从后向前扫描，找到第一个小于tmp的值，填入a[i]
+		while(i < j && a[j] >= tmp) {
+			j--;
+		}
+		if (i < j) { // 找到了
+			a[i++] = a[j];
+		}
+
+		// 从左向右扫描，找一个大于tmp的数，填入a[j]
+		while(i < j && a[i] < tmp) {
+			i++;
+		}
+		if (i < j) {
+			a[j++] = a[i];
+		}
+	}
+
+	// 扫描结束, i == j
+	a[i] = tmp;
+
+	return i;
+}
+
+void quick_sort(int *a, int left, int right) {
+	if (left < right) {
+		int i = partion(a, left, right);
+		quick_sort(a, left, i - 1);
+		quick_sort(a, i + 1, right);
+	}
 }
 
 #define Max_Number 500000
@@ -28,8 +64,8 @@ int main() {
     start = clock();
 	// merge_sort(a,sizeof(a)/sizeof(int));    // 0.002s,可以看到，归并排序还是很快的
 	//heap_sort(a,sizeof(a)/sizeof(int));  // 有buggggggg
-	//quicksort(a,0,sizeof(a)/sizeof(int)-1); // 0.01s
-	insert_sort(a, sizeof(a) / sizeof(int));
+	quick_sort(a, 0, sizeof(a) / sizeof(int) - 1); // 0.01s
+	// insert_sort(a, sizeof(a) / sizeof(int));
 	//select_sort(a,sizeof(a)/sizeof(int));   // 5.3s
 	//bubble_sort(a,sizeof(a)/sizeof(int));    // 12.5s
 	finish = clock();
