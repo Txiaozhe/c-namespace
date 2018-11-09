3260b8dbf06e7624b382ae4e724abd17edbde673
Txiaozhe <txiaozhe@gmail.com>
2018-11-09 09:47:08 +0800

    lambda sort

---
+++ lambda/sort/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 因为使用了iostream 而非iostream.h，所以必须声明命名空间，或者使用 std::cout 的形式
+// using namespace std;
+
+bool cmp(int a, int b) {
+  return a < b;
+}
+
+int main() {
+  std::vector<int> myvec{ 3, 2, 5, 7, 3, 2 };
+  std::vector<int> lbvec(myvec);
+
+  std::sort(lbvec.begin(), lbvec.end(), [](int a, int b) -> bool { return a < b; });
+  std::cout << "lambda expression: " << std::endl;
+  for (int it : lbvec) 
+    std::cout << it << "  ";
+}
