eaccd1e7bb547872a5b46cfb6357181cf0607a88
Txiaozhe <txiaozhe@gmail.com>
2018-11-22 16:31:57 +0800

    mac addr

--- ifaddrs/mask/main.c
+++ ifaddrs/mask/main.c
@@ -3,20 +3,16 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <net/if_dl.h>
 #include <stdbool.h>
+#include <string.h>
 
-struct net_info {
-    char *address;
-    char *netmask;
-    char *family;
-    char *mac;
-    int *scopeid;
-    bool *internal;
-    char *cidr;
-};
-
-int getNameInfo(struct sockaddr *addr, int size_of_saddr_in, char *host) {
-    return getnameinfo(addr, size_of_saddr_in, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+void getNameInfo(struct sockaddr *addr, int size_of_saddr_in, char *host) {
+    int no = getnameinfo(addr, size_of_saddr_in, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+    if (no < 0) 
+    {
+        
+    }
 }
 
 int getSubnetMask()
@@ -31,7 +27,7 @@ int getSubnetMask()
         return -1;
     }
 
-    for (ifa = ifList, i = 0; ifa != NULL; ifa = ifa->ifa_next, i++)
+    for (ifa = ifList, i = 0; ifa != NULL; ifa = ifa->ifa_next, ++i)
     {
         int family = ifa->ifa_addr->sa_family, s;
         char host[NI_MAXHOST];
@@ -45,25 +41,36 @@ int getSubnetMask()
         {
             printf("net: %s, %d\n", ifa->ifa_name, i);
             printf(">>> family: %s\n", "IPv4");
-            s = getNameInfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host);
+            getNameInfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host);
             printf(">>> address: %s\n", host);
-            s = getNameInfo(ifa->ifa_dstaddr, sizeof(struct sockaddr_in), host);
+            getNameInfo(ifa->ifa_dstaddr, sizeof(struct sockaddr_in), host);
             printf(">>> broadcast: %s\n", host);
-            s = getNameInfo(ifa->ifa_netmask, sizeof(struct sockaddr_in), host);
+            getNameInfo(ifa->ifa_netmask, sizeof(struct sockaddr_in), host);
             printf(">>> netmask: %s\n", host);
-            printf("---------------------------------------------------------\n\n");
+            printf("ipv4 ---------------------------------------------------------ipv4 \n\n");
         } else if (family == AF_INET6) {
             printf("net: %s, %d\n", ifa->ifa_name, i);
             printf(">>> family: %s\n", "IPv6");
-            s = getNameInfo(ifa->ifa_addr, sizeof(struct sockaddr_in6), host);
+            getNameInfo(ifa->ifa_addr, sizeof(struct sockaddr_in6), host);
             printf(">>> address: %s\n", host);
-            s = getNameInfo(ifa->ifa_dstaddr, sizeof(struct sockaddr_in6), host);
+            getNameInfo(ifa->ifa_dstaddr, sizeof(struct sockaddr_in6), host);
             printf(">>> broadcast: %s\n", host);
-            s = getNameInfo(ifa->ifa_netmask, sizeof(struct sockaddr_in6), host);
+            getNameInfo(ifa->ifa_netmask, sizeof(struct sockaddr_in6), host);
             printf(">>> netmask: %s\n", host);
-            printf("---------------------------------------------------------\n\n");
+            printf("ipv6 ---------------------------------------------------------ipv6 \n\n");
+        } else if (family == AF_LINK) {
+            struct sockaddr_dl* sdl = (struct sockaddr_dl *)ifa->ifa_addr;
+            unsigned char mac[6];
+            if (6 == sdl->sdl_alen)
+            {
+                memcpy(mac, LLADDR(sdl), sdl->sdl_alen);
+                printf("mac: %s, %d\n", ifa->ifa_name, i);
+                printf(">>> mac: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+                printf("mac ---------------------------------------------------------mac \n\n");
+            }
         } else {
-            // printf("n>>>: %d\n", ifa->ifa_addr->sa_family);
+            printf("other: %d\n", family);
+            printf("other ---------------------------------------------------------other \n\n");
         }
     }
 
---
+++ ifaddrs/mask/net.sh
@@ -0,0 +1,72 @@
+net: lo0, 1
+>>> family: IPv4
+>>> address: 127.0.0.1
+>>> broadcast: 127.0.0.1
+>>> netmask: 255.0.0.0
+ipv4 ---------------------------------------------------------ipv4
+
+net: lo0, 2
+>>> family: IPv6
+>>> address: ::1
+>>> broadcast: ::1
+>>> netmask: ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+ipv6 ---------------------------------------------------------ipv6
+
+net: lo0, 3
+>>> family: IPv6
+>>> address: fe80::1%lo0
+>>> broadcast: fe80::1%lo0
+>>> netmask: ffff:ffff:ffff:ffff::
+ipv6 ---------------------------------------------------------ipv6
+
+mac: en0, 6
+>>> mac: 98:01:a7:8d:0a:89
+mac ---------------------------------------------------------mac
+
+net: en0, 7
+>>> family: IPv6
+>>> address: fe80::1ccd:6342:587b:1b98%en0
+>>> broadcast: fe80::1ccd:6342:587b:1b98%en0
+>>> netmask: ffff:ffff:ffff:ffff::
+ipv6 ---------------------------------------------------------ipv6
+
+net: en0, 8
+>>> family: IPv4
+>>> address: 10.10.0.159
+>>> broadcast: 10.10.1.255
+>>> netmask: 255.255.254.0
+ipv4 ---------------------------------------------------------ipv4
+
+mac: en1, 9
+>>> mac: 6a:00:02:16:c0:b0
+mac ---------------------------------------------------------mac
+
+mac: en2, 10
+>>> mac: 6a:00:02:16:c0:b1
+mac ---------------------------------------------------------mac
+
+mac: bridge0, 11
+>>> mac: 6a:00:02:16:c0:b0
+mac ---------------------------------------------------------mac
+
+mac: p2p0, 12
+>>> mac: 0a:01:a7:8d:0a:89
+mac ---------------------------------------------------------mac
+
+mac: awdl0, 13
+>>> mac: 96:78:71:fc:88:a8
+mac ---------------------------------------------------------mac
+
+net: awdl0, 14
+>>> family: IPv6
+>>> address: fe80::9478:71ff:fefc:88a8%awdl0
+>>> broadcast: fe80::9478:71ff:fefc:88a8%awdl0
+>>> netmask: ffff:ffff:ffff:ffff::
+ipv6 ---------------------------------------------------------ipv6
+
+net: utun0, 16
+>>> family: IPv6
+>>> address: fe80::25f:d102:c97d:55aa%utun0
+>>> broadcast: fe80::25f:d102:c97d:55aa%utun0
+>>> netmask: ffff:ffff:ffff:ffff::
+ipv6 ---------------------------------------------------------ipv6
