154626ebda292da81fbddc3a989b6263f623450f
Txiaozhe <txiaozhe@gmail.com>
2018-12-07 18:16:16 +0800

    format

--- class/void/main.c
+++ class/void/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main () {
-     int a= 10;
+     int a = 10;
      void *b = &a;
      
-    printf("int a = %d\n",a);
-    printf("void (int *)b =%d \n",*(int *)b);
-    printf("void (double *)b =%f \n",*(double*)b);
+    printf("int a = %d\n", a);
+    printf("void (int *)b =%d \n", *(int *)b);
+    printf("void (double *)b =%f \n", *(double*)b);
     return 0;
 }
